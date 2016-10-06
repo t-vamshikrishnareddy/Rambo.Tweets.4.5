aee9b13a99d01a348eaf253327361c590304cdea
Vamshi <t.vamshikrishnareddy@gmail.com>
2016-10-06 14:27:39 +0530

    Add files via upload

---
+++ CADemoPost.App/ViewPost.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CADemoPost.App
+{
+    /// <summary>
+    /// Interaction logic for ViewPost.xaml
+    /// </summary>
+    /// <seealso cref="System.Windows.Window" />
+    /// <seealso cref="System.Windows.Markup.IComponentConnector" />
+    public partial class ViewPost : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewPost"/> class.
+        /// </summary>
+        public ViewPost()
+        {
+            InitializeComponent();
+        }
+    }
+}
