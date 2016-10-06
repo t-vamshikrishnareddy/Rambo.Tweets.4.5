72c6dfb4bb54c27cc841467b88577d1f890cebcf
Vamshi <t.vamshikrishnareddy@gmail.com>
2016-10-06 14:24:19 +0530

    Add files via upload

---
+++ CADemoPost.WebClientTests1/Implementations/WebClientTests.cs
@@ -0,0 +1,19 @@
+﻿using CADemoPost.DTO.Models;
+using CADemoPost.WebClient.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CADemoPost.WebClient.Implementations.Tests
+{
+    [TestClass()]
+    public class WebClientTests
+    {
+        [TestMethod()]
+        public void GetPostsTest()
+        {
+            IWebClient<Post> webClient = new WebClient<Post>(@"http://jsonplaceholder.typicode.com/posts");
+            var list = webClient.GetPosts();
+            
+            Assert.Fail();
+        }
+    }
+}
